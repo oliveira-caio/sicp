ae4418bcf7baa1edddab47f53d2a85fbe1c3ab1b
caio <oliveiracaio1@yandex.com>
2022-01-04 00:17:42 -0300

    first commit

---
+++ ch1/1_3.c
@@ -0,0 +1,25 @@
+int sum_squares(int x, int y, int z)
+{
+  if (x >= y)
+    {
+    if (y >= z)
+      {
+	return x * x + y * y;
+      }
+    else
+      {
+	return x * x + z * z;
+      }
+    }
+  else
+    {
+      if (x >= z)
+	{
+	  return x * x + y * y;
+	}
+      else
+	{
+	  return y * y + z * z;
+	}
+    }
+}
