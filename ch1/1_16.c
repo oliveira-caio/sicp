84fa11f31fdc37618a79f7edb15298765753303f
caio <oliveiracaio1@yandex.com>
2022-01-05 08:59:41 -0300

    Correct ex 1.16

--- ch1/1_16.c
+++ ch1/1_16.c
@@ -1,10 +1,10 @@
 int fast_exp_iter(int base, int power)
 {
-  int ans = base;
+  int ans = 1;
   while (power > 0)
     {
       if (power % 2) ans = base * ans;
-      else ans = ans * ans;
+      base = base * base;
       power = power / 2;
     }
   return ans;
